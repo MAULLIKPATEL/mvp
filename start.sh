8eb14b4d791a32fc0a340b8ad257c778fda4106a
MAULLIKPATEL <119292297+MAULLIKPATEL@users.noreply.github.com>
2023-04-24 20:24:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/MAULLIKPATEL/ShortenerBot-1/tree/try1.git /ShortenerBot-1
+  git clone https://github.com/MAULLIKPATEL/mvp.git /mvp
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /ShortenerBot-1
+  git clone $UPSTREAM_REPO /mvp
 fi
-cd /ShortenerBot-1
+cd /mvp
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
